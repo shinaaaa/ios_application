8f9d15789c5b19faa093199a084c796b517cec6b
shinaaaa <shin029abc@gmail.com>
2021-03-14 14:02:28 +0900

    Swifit UI 연습!!

---
+++ SwiftUI-Practice/SwiftUI-Practice/SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI-Practice
+//
+//  Created by shin kim on 2021/03/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
